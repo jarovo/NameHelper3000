d395ed5ec880667100a7cf5e7ea3bc2989c89aff
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2022-11-02 11:55:16 +0100

    Fixup !

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,22 @@ import { store } from './components/store'
 
 <!-- HTML -->
 <template>
-  <Face v-if="store.current_person"
-  :addressing="store.current_person.addressing"
-  :name="store.current_person.name"
-  :image_name="store.current_person.image_name" />
+  <div class="container text-center">
+    <Face v-if="store.current_person"
+    :addressing="store.current_person.addressing"
+    :name="store.current_person.name"
+    :image_name="store.current_person.image_name" />
 
+    <Play />
 
-  <Play />
+    <footer>
+      <a href="https://mypage.longvu10.repl.co/">
+        <p class="made">Made by K-DEVELOPMENT</p>
+      </a>
+    </footer>
 
-  <div class="item-container">  
-    <a href="https://mypage.longvu10.repl.co/">
-      <p class="made">Made by K-DEVELOPMENT</p>
-    </a>
+    <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet'>
   </div>
-
-  <link href='https://fonts.googleapis.com/css?family=Bangers' rel='stylesheet'>
 </template>
 
 <!-- CSS -->
